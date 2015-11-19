fea8893b2cd5e47d9460c461915103dffa339e91
Bykov Denis <stivenrain@gmail.com>
2015-11-19 08:41:56 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>
+            Быков Денис Станиславович
+        </title>
+        <link href="css/normalize.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet" >        
+        <link href='https://fonts.googleapis.com/css?family=Fira+Sans:400,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    </head>
+    <body>        
+        <main class="main">
+            <div class="main__layout clearfix">
+                <div class="main__avatar">
+                <img src="img/bykov.jpg" width="250" height="256">
+                </div> 
+                <div class="main__content">              
+                    <h1>Быков Денис Станиславович</h1>
+                    <h1>WEB-мастер</h1>
+                    <p>Занимаюсь WEB разработкой более 3-х лет</p>
+                    <ul class="main__skills">
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript/JQuery</li>
+                        <li>PHP</li>
+                        <li>MySQL/MSSQL</li>
+                    </ul>
+                </div>            
+            </div>   
+            <div class="main__layout">
+                <section class="portfolio">  
+                    <h2>Верстка макетов</h2> 
+                    <div class="portfolio__work1">
+                        <h3>Седона</h3>
+                        <ul>
+                            <li>
+                                <img src="img/sedona_index.jpg" width="300" height="647" alt="Главная страница">
+                            </li>
+                            <li>
+                                <img src="img/sedona_inner.jpg" width="300" height="219" alt="Страница каталога">
+                            </li>
+                        </ul>
+                    </div>
+                </section>    
+            </div>
+        </main>
+    </body>
+</html>
