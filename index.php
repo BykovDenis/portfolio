370d91cc58f7d12623217dfb1450c69fd24e0631
Bykov Denis <stivenrain@gmail.com>
2015-11-20 08:30:53 +0300

    добавлены работы резюме

--- index.php
+++ index.php
@@ -21,26 +21,74 @@
                     <ul class="main__skills">
                         <li>HTML5</li>
                         <li>CSS3</li>
+                        <li>Препроцессор SASS</li>
                         <li>JavaScript/JQuery</li>
                         <li>PHP</li>
                         <li>MySQL/MSSQL</li>
                     </ul>
-                </div>            
+                </div> 
+                <div class="main__sertificate">
+                   <h2>Сертификаты</h2>
+                    <ul>
+                        <li>
+                           <img src="img/HTMLAcademy_HTML_CSS.jpg" width="200">
+                        </li>
+                    </ul>
+                </div>           
             </div>   
             <div class="main__layout">
                 <section class="portfolio">  
                     <h2>Верстка макетов</h2> 
-                    <div class="portfolio__work1">
-                        <h3>Седона</h3>
+                    <div class="portfolio__work">
+                        <h3>Sedona</h3>
                         <ul>
                             <li>
-                                <img src="img/sedona_index.jpg" width="300" height="647" alt="Главная страница">
+                                <a href="http://pharminfo.nichost.ru/sedona/index.html" target="_blank">
+                                    <img src="img/sedona_index.jpg" width="300" height="647" alt="Главная страница">
+                                </a>
                             </li>
                             <li>
-                                <img src="img/sedona_inner.jpg" width="300" height="219" alt="Страница каталога">
+                               <a href="http://pharminfo.nichost.ru/sedona/inner.html"  target="_blank">
+                                   <img src="img/sedona_inner.jpg" width="300" height="219" alt="Страница каталога">
+                               </a>
                             </li>
                         </ul>
                     </div>
+                    <div class="portfolio__work">
+                        <h3>Pink</h3>
+                        <ul>
+                            <li>
+                                <a href="http://pharminfo.nichost.ru/pink/index.html" target="_blank">
+                                    <img src="img/pink_index.jpg" width="300" height="1055" alt="Главная страница">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>   
+                    <div class="portfolio__work">
+                        <h3>Jewelery</h3>
+                        <ul>
+                            <li>
+                                <a href="http://pharminfo.nichost.ru/jewelery/index.html" target="_blank">
+                                    <img src="img/jewelery_index.jpg" width="300" height="995" alt="Главная страница">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>    
+                    <div class="portfolio__work">
+                        <h3>BarberShop</h3>
+                        <ul>
+                            <li>
+                                <a href="http://pharminfo.nichost.ru/barbershop/catalog-item.html" target="_blank">
+                                    <img src="img/barbershop_item.jpg" width="300" height="291" alt="Карточка товара">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://pharminfo.nichost.ru/barbershop/catalog-price.html" target="_blank">
+                                    <img src="img/barbershop_price.jpg" width="300" height="265" alt="Прайс-лист">
+                                </a>
+                            </li>                            
+                        </ul>
+                    </div>                                    
                 </section>    
             </div>
         </main>
