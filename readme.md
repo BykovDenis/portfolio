8c906f00376cb9705ab55ba7978753ac3a747822
Denis <stivenrain@gmail.com>
2017-11-24 23:39:49 +0300

    branch1 Fixed footer. Fix React component

--- readme.md
+++ readme.md
@@ -1,6 +1,15 @@
-<h1>Страничка портфлио</h1>
+<h1>Portfolio</h1>
 
 <h2>v 1.0.0</h2>
 <ol>
-  <li>Страничка портфолио построенная на grunt сборщике</li>
+  <li>The page use GRUNT</li>
+</ol>
+<h2>v 2.0.0</h2>
+<ol>
+  <li>The page use Webpack, React </li>
+</ol>
+<h2>v 2.0.1</h2>
+<ol>
+  <li>Fix footer</li>
+  <li>Fix react component. The link open at new tab</li>
 </ol>
--- src/js/components/site-card/index.jsx
+++ src/js/components/site-card/index.jsx
@@ -5,7 +5,7 @@ import styles from './index.scss';
 function SiteCard(props) {
   const data = props.data;
   return (
-    <a className={styles['site-card']} href={data.href}>
+    <a className={styles['site-card']} href={data.href} target="_blank" rel="noreferrer noopener">
       <h3 className={styles['site-card__title']}>{data.name}</h3>
       <div className={styles['site-card__container']}>
         <img className={styles['site-card__img']} src={data.src} width={data.width} height={data.height} alt={data.name} />
